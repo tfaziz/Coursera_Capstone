2ff912502e7ed5878e48716e52d2d1987c3ba1f2
tfaziz <48145633+tfaziz@users.noreply.github.com>
2020-05-16 10:49:07 +0700

    Add files via upload

---
+++ Applied-DataScience-Capstone.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## This is a notebook for Applied Data Science Project
+
+# *This project use Python Programming*
+
+# In[4]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[6]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
